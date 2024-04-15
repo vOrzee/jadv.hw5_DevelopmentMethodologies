00c3316dbd1ca852ab6f14f5385baa06e67a3979
Roman Leshin <vorzee@gmail.com>
2024-04-15 12:20:49 +0500

    Забыл собственно исправить на TreeMap

--- src/main/java/ru/netology/PhoneBook.java
+++ src/main/java/ru/netology/PhoneBook.java
@@ -1,12 +1,9 @@
 package ru.netology;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class PhoneBook {
-    private final Map<String, String> contactsMap = new HashMap<>();
+    private final Map<String, String> contactsMap = new TreeMap<>();
     public int add(String name, String phoneNumber) {
         if (!contactsMap.containsValue(phoneNumber)) {
             contactsMap.put(name, phoneNumber);
