2c73181387341599de2f2c4ea45809d6b677fc2a
Roman Leshin <vorzee@gmail.com>
2024-04-15 11:54:36 +0500

    Stub and tests for add

--- src/main/java/ru/netology/PhoneBook.java
+++ src/main/java/ru/netology/PhoneBook.java
@@ -1,5 +1,11 @@
 package ru.netology;
 
-public class PhoneBook {
+import java.util.HashMap;
+import java.util.Map;
 
+public class PhoneBook {
+    private final Map<String, String> contactsMap = new HashMap<>();
+    public int add(String name, String phoneNumber) {
+        return 0;
+    }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,4 +1,29 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import ru.netology.PhoneBook;
 
 public class PhoneBookTest {
+
+    static PhoneBook phoneBook;
+
+    @BeforeEach
+    public void setup() {
+        phoneBook = new PhoneBook();
+        phoneBook.add("Елена", "89261234567");
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "Иван, 89033004567",
+            "Мария, 89033004568",
+            "Андрей, 89033004569"
+    })
+    public void addTest(String name, String number) {
+        int expected = 1;
+        int actual = phoneBook.add(name, number);
+        Assertions.assertEquals(1, actual, "После добавления в пустую книгу размер должен быть равен одному");
+    }
 
 }
